08065ae371f0a945d53bbb1ce61e71a32d88239b
Ordeeep <pedroivan12@live.com>
2023-03-07 08:56:54 -0300

    1001

--- src/ExtremamenteBasico.java
+++ src/ExtremamenteBasico.java
@@ -1,11 +1,18 @@
 import java.util.Scanner;
 
+//Lembre-se de alterar a classe "ExtremamenteBasico" para Main
 public class ExtremamenteBasico {
     public static void main(String[] args) {
-        Scanner leitor = new Scanner(System.in);
-        int a = leitor.nextInt();
-        int b = leitor.nextInt();
-        int x = a + b;
-        System.out.println("X = " + x);
+        Scanner input = new Scanner(System.in);
+        // Declaração das variaveis
+        int A, B, X;
+        // Entrada de dados
+        A = input.nextInt();
+        B = input.nextInt();
+        // Tradando os dados
+        X = A + B;
+        // Saida
+        System.out.println("X = " + X);
+
     }
 }
