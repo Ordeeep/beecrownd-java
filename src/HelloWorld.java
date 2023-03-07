d4ccbc4696527dba3b73031520437393d620889b
Ordeeep <pedroivan12@live.com>
2023-03-07 09:30:33 -0300

    1000 - Hello World!

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,4 +1,4 @@
-//Lembre-se de alterar o nome da class para Main
+//Lembre-se de alterar o nome da Classe para Main
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
